e30f2c77389dbd438d5bc5c63b29f595bc756a33
Renato Rodrigues de Araujo <renato.araujo@mytoys.de>
2020-06-17 19:21:57 +0200

    Change the SMS message

--- src/Command/FindAnmeldungSlotsCommand.php
+++ src/Command/FindAnmeldungSlotsCommand.php
@@ -39,7 +39,7 @@ class FindAnmeldungSlotsCommand extends Command
 
         try {
             if ($this->slotFinder->isAnySlotAvailable()) {
-                $message = 'Slot for Anmeldung found, please head NOW to https://service.berlin.de/terminvereinbarung/termin/day/';
+                $message = 'Slot for Anmeldung found! Please head NOW to https://bit.ly/3fAevG4 and remember to open this page in clear/anonymous tab because of the session.';
                 $this->smsClient->send($message);
                 $output->writeln($message);
                 return Command::SUCCESS;
